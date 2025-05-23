f3069edbc46808236d4deff0c1053622486334e6
Matheus Pegorari <pegorari42@gmail.com>
2025-05-22 22:56:51 -0300

    refactor: changed name of company

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import UsinasTable, { Usina } from "@/app/usinas-table";
 import { Button } from "@/components/ui/button";
 import { Loader2, Search } from "lucide-react";
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
@@ -12,7 +11,7 @@ export default function Home() {
   const fetchUsinas = async () => {
     setLoading(true);
     const response = await fetch(
-      "https://boltenergy.pegorari.tech/api/v1/usinas/top"
+      "https://api-ralie.pegorari.tech/api/v1/usinas/top"
     );
     const data: Usina[] = await response.json();
     setUsinas(data);
@@ -21,10 +20,20 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="flex text-4xl font-bold justify-center items-center">
-          <Image src="/image.jpg" alt="Bolt Energy" width={100} height={100} />
-          Energy</h1>
+        <h1 className="flex text-4xl w-full font-bold justify-center items-center text-center ">
+          RALIE
+        </h1>
 
+        <div className="text-center max-w-4xl">
+          <p className="text-lg font-medium text-gray-700 dark:text-gray-300 mb-2">
+            Relatório de Acompanhamento da Expansão da Oferta de Geração de
+            Energia Elétrica
+          </p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            ralie-usina.csv - Dados Abertos - Agência Nacional de Energia
+            Elétrica
+          </p>
+        </div>
         <div className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           Clique no botão abaixo para visualizar as <br />
           <span className="font-bold">Top 5 usinas de Energia do Brasil.</span>
